b16651a207a56a460bc8e0e4b8ccad8a581dd220
Douglas Costa <douglasaquinocosta@gmail.com>
2021-10-20 17:10:17 -0300

    finalizando o backend

--- src/routes.ts
+++ src/routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express'
 import { AuthenticateUserController } from './controller/AuthenticateUserController'
 import { CreateMessageControler } from './controller/CreateMessageController'
+import { Get3LastMessagesController } from './controller/GetLast3MessagesController'
+import { ProfileUserController } from './controller/ProfileUserController'
 import { ensureAuthenticated } from './middleware/ensureAuthenticated'
 
 const router = Router()
@@ -12,5 +14,9 @@ router.post(
   ensureAuthenticated,
   new CreateMessageControler().handle
 )
+
+router.get('/messages/last3', new Get3LastMessagesController().handle)
+
+router.get('/profile', ensureAuthenticated, new ProfileUserController().handle)
 
 export { router }
